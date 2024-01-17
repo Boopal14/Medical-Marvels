e5a84af06817f36190b03560209cd9ab281f0200
Boopal N <151910545+Boopal14@users.noreply.github.com>
2024-01-17 18:51:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Message sent successfully!");
+    // You can add further functionality, like sending data to a server, here.
+});
